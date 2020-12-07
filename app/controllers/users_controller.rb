d4c537be8b499f62592d3f9def0dba22873a632e
Igors78 <oleinikovsigors@gmail.com>
2020-12-07 15:02:35 +0200

    Add edit and update actions, error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,18 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+  end
 end
