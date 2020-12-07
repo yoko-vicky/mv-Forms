199aaa9fe221a6b7ccdfd07d2cfe0a47e3761170
yocosaka <sakayoco55@gmail.com>
2020-12-07 21:35:53 +0900

    Add create and new methods, and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
-  def new; end
+  include UsersHelper
 
-  def create; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
